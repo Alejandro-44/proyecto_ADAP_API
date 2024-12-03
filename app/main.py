f25e27ddc1f8bfef9848711d912558146954e605
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2024-12-03 18:37:39 -0500

    Agregando comando de arranque para pro

--- app/main.py
+++ app/main.py
@@ -38,3 +38,8 @@ app.include_router(dashboard.router)
 @app.get("/")
 async def root(request: Request):
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))  # Usa el puerto proporcionado por Railway
+    uvicorn.run(app, host="0.0.0.0", port=port)
