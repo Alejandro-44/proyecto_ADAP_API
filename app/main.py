22d3f6fb09add0ff95ef14a9f9e84feb7b6f3a00
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2024-12-03 18:37:39 -0500

    Agregando comando de arranque para pro

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, Request
 # local packages
 from app import models
@@ -38,3 +39,8 @@ app.include_router(dashboard.router)
 @app.get("/")
 async def root(request: Request):
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))  # Usa el puerto proporcionado por Railway
+    uvicorn.run(app, host="0.0.0.0", port=port)
