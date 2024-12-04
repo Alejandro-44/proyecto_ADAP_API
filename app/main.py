6aa5095b1b80be90dc87d689443c95811a6e041b
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2024-12-03 23:34:24 -0500

    Actualizando configuracion de host

--- app/main.py
+++ app/main.py
@@ -49,4 +49,4 @@ async def root(request: Request):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))  # Usa el puerto proporcionado por Railway
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
