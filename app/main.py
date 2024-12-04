3e4975af7e7c1b44b482862e7dcedeeb316fd8c6
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2024-12-04 00:12:17 -0500

    Corrigiendo permisos de middleware

--- app/main.py
+++ app/main.py
@@ -23,7 +23,9 @@ origins = [
     "http://localhost:5173",  
     "http://127.0.0.1:5173",   
     "http://localhost:3000",  
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3000",   
+    "https://proyecto_adap_app.railway.internal",
+    "https://proyecto_adap_api.railway.internal",
     "https://proyectoadapapp-production.up.railway.app",
     "https://proyectoadapapi-production.up.railway.app"
 ]
